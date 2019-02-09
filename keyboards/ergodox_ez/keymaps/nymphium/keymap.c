216d585bc8517901d8afacbab4c34df55f928ba0
Kawahara Satoru <s1311350@gmail.com>
2019-02-09 17:58:13 +0900

    update

--- keyboards/ergodox_ez/keymaps/nymphium/keymap.c
+++ keyboards/ergodox_ez/keymaps/nymphium/keymap.c
@@ -8,6 +8,7 @@
 #define _MOUSE 4
 #define _WHEEL 5
 #define _OMOUSE 6
+#define _XF86 7
 
 #define ___ _______
 
@@ -21,7 +22,6 @@
       return false; \
       break;
 
-
 #define led_on(x) ergodox_right_led_##x##_on()
 #define led_off(x) ergodox_right_led_##x##_off()
 
@@ -32,7 +32,8 @@ enum custom_keycodes {
   LSHIFT,
   MOUSE,
   WHEEL,
-  OMOUSE
+  OMOUSE,
+  XF86
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -60,48 +61,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [BASE] = LAYOUT_ergodox(
   // left hand
-  //,-----------------------------------------------------------------------------.
-  /*|*/ KC_ESC, /*|*/KC_1,/*|*/KC_2,/*|*/KC_3,   /*|*/KC_4, /*|*/KC_5,/*|*/KC_6,//|
-  //|-------------+---------+---------+------------+----------+---------+---------|
-  /*|*/ KC_TAB, /*|*/KC_Q,/*|*/KC_W,/*|*/KC_E,   /*|*/KC_R, /*|*/KC_T,/*|*/KC_Y,//|
-  //|-------------+---------+---------+------------+----------+---------+---------|
-  /*|*/ KC_ENT, /*|*/KC_A,/*|*/KC_S,/*|*/KC_D,   /*|*/KC_F, /*|*/KC_G,//          |
-  //|-------------+---------+---------+------------+----------+---------+---------|
-  /*|*/ KC_LSFT,/*|*/KC_Z,/*|*/KC_X,/*|*/KC_C,   /*|*/KC_V, /*|*/KC_B,/*|*/___, //|
-  //`-------------+---------+---------+------------+----------+-------------------'
-  /*  |*/ ___,  /*|*/___, /*|*/___, /*|*/KC_LALT,/*|*/LOWER,//|
-  //  `-------------------------------------------------------'
+   XF86, KC_1, KC_2,KC_3,   KC_4, KC_5,KC_6,
+   KC_TAB, KC_Q, KC_W,KC_E,   KC_R, KC_T,KC_Y,
+   KC_ENT, KC_A, KC_S,KC_D,   KC_F, KC_G,
+   KC_LSFT,KC_Z, KC_X,KC_C,   KC_V, KC_B,KC_Y,
+   ___, ___,KC_LGUI,KC_LALT,LOWER,
 
-                                                     // left thumb
-                                         //            ,--------------------------.
-                                         /*            |*/KC_LEFT,/*|*/KC_UP,   //|
-                                         //,-----------|------------+-------------|
-                                         /*|           |            |*/KC_DOWN, //|
-                                         //|           |            |-------------|
-                                         /*|*/KC_SPC,/*|*/ LOWER, /*|*/KC_RIGHT,//|
-                                         //`--------------------------------------'
+                                                 // left thumb
+                                         KC_LEFT,  KC_UP,
+                                                          KC_DOWN,
+                                         KC_SPC,   LOWER, KC_RIGHT,
 
   // right hand
-  //,-----------------------------------------------------------------------------------.
-  /*|*/RESET,/*|*/KC_7,/*|*/KC_8, /*|*/KC_9,/*|*/KC_0,/*|*/___,    /*|*/___,          //|
-  //|----------+---------+----------+---------+---------+------------+------------------|
-  /*|*/KC_Y, /*|*/KC_Y,/*|*/KC_U, /*|*/KC_I,/*|*/KC_O,/*|*/KC_P,   /*|*/KC_BSLS,      //|
-  //|----------+---------+----------+---------+---------+------------+------------------|
-  /*|*/      /*|*/KC_H,/*|*/KC_J, /*|*/KC_K,/*|*/KC_L,/*|*/KC_MHEN,/*|*/___,          //|
-  //|----------+---------+----------+---------+---------+------------+------------------|
-  /*|*/KC_B, /*|*/KC_N,/*|*/KC_M, /*|*/___, /*|*/___, /*|*/___,    /*|*/___,          //|
-  //`--------------------+----------+---------+---------+------------+------------------'
-  /*                     |*/MOUSE,/*|*/___, /*|*/___, /*|*/___,    /*|*/TG(_OMOUSE),//|
-  //                     `------------------------------------------------------------'
+  RESET,KC_7,KC_8,    KC_9,KC_0,___,    ___,
+  KC_Y, KC_Y,KC_U,    KC_I,KC_O,KC_P,   KC_BSLS,
+        KC_H,KC_J,    KC_K,KC_L,KC_MHEN,___,
+  KC_B, KC_N,KC_M,    ___, ___, ___,    ___,
+  KC_LCTRL,___, ___, ___,    TG(_OMOUSE),
 
   // right thumb
-  //,-------------------------.
-  /*|*/KC_MS_U,/*|*/KC_MS_R,//|
-  //|------------+------------|-----------.
-  /*|*/KC_MS_D,//|            |           |
-  //|------------|            |           |
-  /*|*/KC_MS_L,/*|*/KC_BSPC,/*|*/KC_LCTL//|
-  //`-------------------------------------'
+  KC_MS_U,        KC_MS_R,
+  KC_MS_D,
+  KC_MS_L,MOUSE,  KC_BSPC
 )
 
 ,[_LOWER] = LAYOUT_ergodox(
@@ -125,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   ___,      ___,
   ___,
-  ___,      KC_DELETE,  ___
+  ___,      ___, KC_DELETE
 )
 
 ,[_SHIFT] = LAYOUT_ergodox(
@@ -155,7 +136,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ,[_LSHIFT] = LAYOUT_ergodox(
   // left hand
   ___,    ___,   ___,  ___, ___, ___,  ___,
-  ___,    KC_GRV,   KC_ASTR,     ___, KC_COLON,  KC_TILDE,  KC_7,
+  ___,    KC_GRV,  KC_ASTR,     ___, KC_COLON,  KC_TILDE,  KC_7,
   ___,    KC_EQL,   ___,     KC_UNDS,  KC_PIPE,  KC_AMPR,
   ___,    ___,   ___,     ___,  ___,  ___,  ___,
   ___,    ___,   ___,     ___,  ___,
@@ -180,9 +161,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // left hand
   ___,    ___,   ___,  ___,  ___,  ___,  ___,
   ___,    ___,   ___,  ___,  ___,  ___,  ___,
-  ___,    ___,   ___,  ___,  ___,  WHEEL,
+  ___,    ___,   ___,  ___,  WHEEL,  ___,
   ___,    ___,   ___,  ___,  ___,  ___,  ___,
-  ___,    ___,   KC_BTN1, KC_BTN3, KC_BTN2,
+  ___,    ___,   KC_BTN1, WHEEL, KC_BTN2,
 
                                     ___, ___,
                                          ___,
@@ -247,29 +228,79 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ___,
   ___, ___, ___
 )
+
+,[_XF86] = LAYOUT_ergodox(
+  // left hand
+  ___,   KC_MUTE,  KC_VOLD,  KC_VOLU,  ___, ___,  ___,
+  ___,    ___,   ___,     ___,  ___,  ___,  ___,
+  ___,    ___,   ___,     ___,  ___,  ___,
+  ___,    ___,   ___,     ___,  ___,  ___,  ___,
+  ___,    ___,   ___,     ___,  ___,
+
+                                    ___, ___,
+                                         ___,
+                               ___, ___, ___,
+
+  // right hand
+  ___, ___,  ___, ___, ___,  ___,  ___,
+  ___, ___,  ___, ___, KC_BRID, KC_BRIU,  ___,
+       ___, ___, ___, ___,  ___,  ___,
+  ___, ___,  ___, ___, ___,  ___,  ___,
+            ___, KC_PSCR,  ___, ___, ___,
+
+  ___, ___,
+  ___,
+  ___, ___, ___
+)
 };
 
+static bool middle_tap1;
+static bool henkan;
+static bool shift;
+
+static bool modkeyed = false;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
   switch (keycode) {
-    TMP_MODE(MOUSE);
-    TMP_MODE(WHEEL);
+    case KC_LGUI:
+    case KC_LALT:
+      if (record->event.pressed) {
+        register_code(keycode);
+        modkeyed = true;
+      } else {
+        unregister_code(keycode);
+        modkeyed = false;
+      }
+      return false;
 
     case KC_GRV:
     case KC_EQL:
-    case KC_ASTR:
+    /* case KC_ASTR: */
     case KC_7:
-      unregister_code(KC_LSFT);
-
+      if (record->event.pressed) {
+        unregister_code(KC_LSFT);
+        register_code(keycode);
+      } else {
+        if (shift) register_code(KC_LSFT);
+      }
       return true;
 
     case KC_LSFT:
       if (record->event.pressed) {
         register_code(KC_LSFT);
-        layer_on(_SHIFT);
-        update_tri_layer(_LOWER, _SHIFT, _LSHIFT);
+
+        if (!modkeyed) {
+          shift = true;
+          layer_on(_SHIFT);
+          update_tri_layer(_LOWER, _SHIFT, _LSHIFT);
+        }
       } else {
-        layer_off(_SHIFT);
-        update_tri_layer(_LOWER, _SHIFT, _LSHIFT);
+        if (!modkeyed) {
+          shift = false;
+          layer_off(_SHIFT);
+          update_tri_layer(_LOWER, _SHIFT, _LSHIFT);
+        }
         unregister_code(KC_LSFT);
       }
       return false;
@@ -294,6 +325,52 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
 
       return false;
+
+    case WHEEL:
+      if (record->event.pressed) {
+        if (middle_tap1) {
+          layer_on(_WHEEL);
+          middle_tap1 = false;
+        } else {
+          register_code(KC_BTN3);
+          middle_tap1 = true;
+        }
+      } else {
+        led_off(1);
+        if (middle_tap1) {
+          unregister_code(KC_BTN3);
+        } else {
+          layer_off(_WHEEL);
+        }
+      }
+
+      return false;
+
+    case KC_MHEN:
+      if (record->event.pressed) {
+        if(henkan) led_off(2);
+        henkan = false;
+        register_code(keycode);
+      } else {
+        unregister_code(keycode);
+      }
+
+      return false;
+
+    case KC_HENK:
+      if (record->event.pressed) {
+        henkan = true;
+        led_on(2);
+        register_code(keycode);
+      } else {
+        led_on(2);
+        unregister_code(keycode);
+      }
+
+      return false;
+
+    TMP_MODE(XF86);
+    TMP_MODE(MOUSE);
   }
 
   return true;
@@ -304,7 +381,7 @@ void matrix_init_user(void) {
 #ifdef RGBLIGHT_COLOR_LAYER_0
   rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
 #endif
-};
+}
 
 uint32_t layer_state_set_user(uint32_t state) {
   ergodox_board_led_off();
@@ -314,14 +391,25 @@ uint32_t layer_state_set_user(uint32_t state) {
 
   uint8_t layer = biton32(state);
   switch (layer) {
+    case BASE:
+    case _SHIFT:
+    case _LSHIFT:
+    case _LOWER:
+      if (henkan) {
+        led_on(2);
+      } else {
+        led_off(2);
+      }
+      break;
+
+    case _MOUSE:
     case _OMOUSE:
       led_on(3);
-      #ifdef RGBLIGHT_COLOR_LAYER_0
-        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
-      #endif
+      middle_tap1 = false;
       break;
 
     case _WHEEL:
+      led_on(3);
       led_on(1);
       break;
     }
